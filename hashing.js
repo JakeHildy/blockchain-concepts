73468c22bf6410eca106a93b0830c93d655138e0
Jacob Hildebrandt <jake@hildy.ca>
2021-05-12 20:38:00 -0700

    Initial Commit

---
+++ hashing.js
@@ -0,0 +1,19 @@
+// const sha256 = require("sha256");
+// const hash = sha256("blockchain Nanodegree Program");
+
+const SHA256 = require("crypto-js/sha256");
+
+const data1 = "Blockchain Rock!";
+const dataObject = {
+  id: 1,
+  body: "With Object Works too",
+  time: new Date().getTime().toString().slice(0, -3),
+};
+
+function generateHash(obj) {
+  return SHA256(JSON.stringify(obj));
+}
+
+console.log(`SHA256 Hash: ${generateHash(data1)}`);
+console.log("************************************");
+console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
